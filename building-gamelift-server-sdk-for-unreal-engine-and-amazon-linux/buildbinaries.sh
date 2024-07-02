f52e93b94e1533d3fae334baa996a5baab5e3439
juhoaws <jjantun@amazon.com>
2024-07-02 11:23:38 -0700

    WIP: staged initial version of the tool

---
+++ building-gamelift-server-sdk-for-unreal-engine-and-amazon-linux/buildbinaries.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Building the Unreal GameLift Server SDK binaries for Amazon Linux 2023..."
+docker buildx build --platform=linux/amd64 --output=./ --target=server .
+echo "Done, now zipping the content.."
+zip -r AL2023GameliftUE5sdk.zip lib*
+echo "Done! Select Actions -> Download File and type AL2023GameliftUE5sdk.zip to download the binaries."
