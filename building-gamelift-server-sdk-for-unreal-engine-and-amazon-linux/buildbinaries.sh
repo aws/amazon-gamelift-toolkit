01eb785152b6b34f43b06835a56174b446520811
juhoaws <60425750+juhoaws@users.noreply.github.com>
2025-01-07 08:09:54 -0800

    Update buildbinaries.sh

--- building-gamelift-server-sdk-for-unreal-engine-and-amazon-linux/buildbinaries.sh
+++ building-gamelift-server-sdk-for-unreal-engine-and-amazon-linux/buildbinaries.sh
@@ -4,4 +4,4 @@ echo "Building the Unreal GameLift Server SDK binaries for Amazon Linux 2023..."
 docker buildx build --platform=linux/amd64 --output=./ --target=server .
 echo "Done, now zipping the content.."
 zip -r AL2023GameliftUE5sdk.zip lib*
-echo "Done! Select Actions -> Download File and type AL2023GameliftUE5sdk.zip to download the binaries."
+echo "Done! Select Actions -> Download File and type /home/cloudshell-user/amazon-gamelift-toolkit/building-gamelift-server-sdk-for-unreal-engine-and-amazon-linux/AL2023GameliftUE5sdk.zip to download the binaries."
